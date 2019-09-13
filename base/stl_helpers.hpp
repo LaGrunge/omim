2abcd5a276016365d9f1b763ce5aa583ac923c4a
LaGrunge <l.fedorov@corp.mail.ru>
2019-09-13 13:21:48 +0000

    move erase_if to stlhelpers

--- base/stl_helpers.hpp
+++ base/stl_helpers.hpp
@@ -442,3 +442,32 @@ struct EnumClassHash
   }
 };
 }  // namespace base
+
+
+#if defined(__GNUC__) && !defined(__clang__)
+#include <experimental/unordered_map>
+
+namespace std
+{
+using std::experimental::erase_if;
+}
+#else
+namespace std
+{
+template <class Key, class T, class Hash, class KeyEqual, class Alloc, class Pred>
+void erase_if(std::unordered_map<Key, T, Hash, KeyEqual, Alloc> & c, Pred pred)
+{
+  for (auto i = c.begin(), last = c.end(); i != last;)
+  {
+    if (pred(*i))
+    {
+      i = c.erase(i);
+    }
+    else
+    {
+      ++i;
+    }
+  }
+}
+}  // namespace
+#endif
